844775f36c77408afa0137c97cf4854c375385a2
T8RIN <oolbp@bk.ru>
2023-07-28 15:09:12 +0300

    ref

--- feature/filters/src/main/java/com/t8rin/imagetoolboxlite/feature/filters/data/FavoriteFiltersInteractorImpl.kt
+++ feature/filters/src/main/java/com/t8rin/imagetoolboxlite/feature/filters/data/FavoriteFiltersInteractorImpl.kt
@@ -41,6 +41,7 @@ internal class FavoriteFiltersInteractorImpl @Inject constructor(
         prefs[FAVORITE_FILTERS]?.toFiltersList() ?: emptyList()
     }
 
+
     override suspend fun toggleFavorite(filter: Filter<Bitmap, *>) {
         val currentFilters = getFavoriteFilters().first()
         if (currentFilters.filterIsInstance(filter::class.java).isEmpty()) {
